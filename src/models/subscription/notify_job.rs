890226f82cfb24d2e5eec820b229e98f5e7edd3b
Nicolas del Valle <nicolas.delvalle@gmail.com>
2022-03-04 20:00:32 -0300

    Update queues implementation

--- src/models/subscription/notify_job.rs
+++ src/models/subscription/notify_job.rs
@@ -1,36 +1,54 @@
+use bson::oid::ObjectId;
 use lapin::message::DeliveryResult;
 use lapin::options::BasicAckOptions;
 use tracing::{error, info};
 
 use crate::errors::Error;
 use crate::messenger::Messenger;
+use crate::models::subscription::Model as SubscriptionModel;
 
-pub async fn setup(messenger: Messenger) -> Result<(), Error> {
+pub async fn setup(subscription: SubscriptionModel, messenger: Messenger) -> Result<(), Error> {
   let queue_name = "send_webhook_event";
   let _queue = messenger.create_queue(queue_name).await.unwrap();
   let consumer = messenger.create_consumer(queue_name).await.unwrap();
 
-  consumer.set_delegate(move |delivery: DeliveryResult| async move {
-    info!("Processing send_webhook_event message");
+  consumer.set_delegate(move |delivery: DeliveryResult| {
+    let subscription = subscription.clone();
 
-    let delivery = match delivery {
-      // Carries the delivery alongside its channel
-      Ok(Some(delivery)) => delivery,
-      // The consumer got canceled
-      Ok(None) => return,
-      // Carries the error and is always followed by Ok(None)
-      Err(error) => {
-        error!("Failed to consume send_webhook_event message {}", error);
-        return;
-      }
-    };
+    async move {
+      info!("Processing send_webhook_event message");
 
-    // Do work
+      let delivery = match delivery {
+        // Carries the delivery alongside its channel
+        Ok(Some(delivery)) => delivery,
+        // The consumer got canceled
+        Ok(None) => return,
+        // Carries the error and is always followed by Ok(None)
+        Err(error) => {
+          error!("Failed to consume send_webhook_event message {}", error);
+          return;
+        }
+      };
 
-    delivery
-      .ack(BasicAckOptions::default())
-      .await
-      .expect("Failed to ack send_webhook_event message");
+      let data = delivery.data.clone();
+      let data: [u8; 12] = match data.as_slice().try_into() {
+        Ok(value) => value,
+        Err(_) => {
+          error!("Failed to parse send_webhook_event payload");
+          return;
+        }
+      };
+
+      let subscription_id = ObjectId::from_bytes(data);
+
+      info!("Syncing subscription with ID {}", &subscription_id);
+      subscription.notify(subscription_id).await.unwrap();
+
+      delivery
+        .ack(BasicAckOptions::default())
+        .await
+        .expect("Failed to ack send_webhook_event message");
+    }
   });
 
   Ok(())
