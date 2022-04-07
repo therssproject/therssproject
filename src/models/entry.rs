2f5a4a0f12a875f13713acf8622d0fa1b0891c2a
Nicolas del Valle <nicolas.delvalle@gmail.com>
2022-04-06 20:31:19 -0500

    Update Entry and Feed models

--- src/models/entry.rs
+++ src/models/entry.rs
@@ -40,17 +40,29 @@ pub struct Entry {
   pub id: Option<ObjectId>,
   pub feed: ObjectId,
   pub public_id: String,
+  pub url: Option<String>,
   pub title: Option<String>,
+  pub description: Option<String>,
+  pub published_at: Option<Date>,
   pub created_at: Date,
 }
 
 impl Entry {
   pub fn from_raw_entry(feed: ObjectId, raw_entry: RawEntry) -> Self {
+    let url = raw_entry.links.get(0).map(|link| link.href.clone());
+
+    let title = raw_entry.title.clone().map(|title| title.content);
+    let description = raw_entry.summary.clone().map(|summary| summary.content);
+    let published_at = raw_entry.published.map(|published| published.into());
+
     Self {
       id: None,
       feed,
       public_id: raw_entry.id,
-      title: None,
+      url,
+      title,
+      description,
+      published_at,
       created_at: now(),
     }
   }
