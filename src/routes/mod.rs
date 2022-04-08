12888bf9c21adff231265d1630ade5c8cac3b3fd
Nicolas del Valle <nicolas.delvalle@gmail.com>
2022-04-07 20:27:42 -0500

    Update router to nest resources under application

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -3,3 +3,20 @@ pub mod feed;
 pub mod subscription;
 pub mod user;
 pub mod webhook;
+
+use axum::Router;
+
+pub fn create() -> Router {
+  Router::new()
+    .merge(user::create_route())
+    .merge(feed::create_route())
+    .merge(
+      Router::new().nest(
+        "/application/:application_id",
+        Router::new()
+          .merge(application::create_route())
+          .merge(subscription::create_route())
+          .merge(webhook::create_route()),
+      ),
+    )
+}
