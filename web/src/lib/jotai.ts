e7d4228ddcbfe1c8493d8986cf723895b35c9e51
gillchristian <gillchristiang@gmail.com>
2022-05-09 10:16:33 +0200

    Fmt fix

--- web/src/lib/jotai.ts
+++ web/src/lib/jotai.ts
@@ -1,11 +1,11 @@
-import * as jotai from 'jotai'
-import {SetAtom} from 'jotai/core/atom'
+import * as jotai from 'jotai';
+import {SetAtom} from 'jotai/core/atom';
 
-type Awaited<T> = T extends Promise<infer V> ? V : T
+type Awaited<T> = T extends Promise<infer V> ? V : T;
 
 // At the moment the inference for the state part of the `useAtom` broken =/
 // It infers to `any` / `unknown`
 // With this wrapper we get the right type on the state
 export const useAtom: <Value, Result extends void | Promise<void>>(
   atom: jotai.Atom<Value>,
-) => [Awaited<Value>, SetAtom<Value, Result>] = jotai.useAtom
+) => [Awaited<Value>, SetAtom<Value, Result>] = jotai.useAtom;
