2b59fa62de45f4242a02f309cf4535c71db5b354
Nicolas del Valle <nicolas.delvalle@gmail.com>
2022-05-15 00:44:32 -0300

    Update scheduler logic

--- api/src/scheduler.rs
+++ api/src/scheduler.rs
@@ -2,7 +2,6 @@ use bson::doc;
 use bson::oid::ObjectId;
 use chrono::{Duration, Utc};
 use futures::future;
-use futures::stream;
 use futures::StreamExt;
 use lazy_static::lazy_static;
 use tokio::time::sleep;
@@ -13,7 +12,7 @@ use wither::WitherError;
 
 use crate::errors::Error;
 use crate::lib::database_model::ModelExt;
-use crate::messenger::Messenger;
+use crate::messenger::get_messenger;
 use crate::models::feed::Feed;
 use crate::models::subscription::Subscription;
 
@@ -21,7 +20,9 @@ lazy_static! {
   static ref SCHEDULER_INTERVAL: Duration = Duration::minutes(5);
 }
 
-pub fn start(messenger: Messenger) {
+pub fn start() {
+  let messenger = get_messenger();
+
   tokio::spawn(async move {
     loop {
       info!("Running scheduler");
@@ -37,68 +38,40 @@ pub fn start(messenger: Messenger) {
         }
       };
 
-      let res = feeds
+      feeds
         .filter_map(parse)
         .map(sync_feed)
         .buffer_unordered(concurrency)
         // Filter out feeds that failed to sync.
-        .filter_map(|feed_id| future::ready(feed_id))
-        .map(|feed_id| async move {
-          let cursor = match find_subscription(feed_id).await {
+        .filter_map(future::ready)
+        .for_each_concurrent(concurrency, |feed_id| async move {
+          let cursor = Subscription::cursor(doc! { "feed": feed_id }, None).await;
+          let cursor = match cursor {
             Ok(cursor) => cursor,
             Err(error) => {
-              error!("Failed to fetch subscriptions for feed: {}", error);
-              return None;
+              error!(
+                "Failed to fetch subscriptions for feed: {}. Error {}",
+                feed_id, error
+              );
+              return;
             }
           };
 
-          Some(())
-        });
-        // .then(find_subscription)
-        // .map(|sa| {
-        //   let subscription = 
-        // });
-      // .flat_map(|feed_id| Subscription::cursor(doc! { "feed": feed_id }, None));
-      // .flat_map(|feed_id| stream::iter(page));
-
-      // let res = feeds
-      //   .filter_map(parse)
-      //   .map(sync_feed)
-      //   .buffer_unordered(concurrency)
-      //   // Filter out feeds that failed to sync.
-      //   .filter(|feed_id| future::ready(feed_id.is_some()))
-      //   // .flat_map(|feed_id| stream::iter(page));
-
-      // // TODO: Crear funcion que se encargue de todo esto, sino ya es un kilombo
-      // .then(|feed_id| Subscription::cursor(doc! { "feed": feed_id }, None))
-      // // .buffer_unordered(concurrency)
-      // .flatten()
-      // // .flatten()
-      // .filter_map(|feed| {
-      //   let result = match feed {
-      //     Ok(feed) => Some(feed),
-      //     Err(err) => {
-      //       error!(
-      //         "Failed to parse MongoDB document into Feed model: {:?}",
-      //         err
-      //       );
-      //       None
-      //     }
-      //   };
-      //   async move { result }
-      // })
-      // .for_each_concurrent(concurrency, |subscription| {
-      //   let messenger = messenger.clone();
-      //   let id = subscription.id.unwrap();
-
-      //   async move {
-      //     messenger
-      //       .publish("send_webhook_event", id.bytes().as_ref())
-      //       .await
-      //       .unwrap();
-      //   }
-      // })
-      // .await;
+          cursor
+            .for_each(|subscription| {
+              let subscription = subscription.unwrap();
+              let id = subscription.id.unwrap();
+              async move {
+                messenger
+                  .publish("send_webhook_event", id.bytes().as_ref())
+                  .await
+                  .unwrap();
+              }
+            })
+            .await;
+          // TODO: Update feed last sync attribute.
+        })
+        .await;
 
       sleep(SCHEDULER_INTERVAL.to_std().unwrap()).await;
     }
@@ -116,11 +89,6 @@ async fn find_feeds() -> Result<Cursor<Feed>, Error> {
   Feed::cursor(query, None).await
 }
 
-async fn find_subscription(feed_id: ObjectId) -> Result<Cursor<Subscription>, Error> {
-  let query = doc! { "feed": feed_id };
-  Subscription::cursor(query, None).await
-}
-
 async fn sync_feed(feed: Feed) -> Option<ObjectId> {
   let id = feed.id.unwrap();
   let url = feed.url;
@@ -133,7 +101,7 @@ async fn sync_feed(feed: Feed) -> Option<ObjectId> {
     Ok(_) => Some(id),
     Err(error) => {
       error!("Failed to sync Feed with ID {:?}. Error: {}", id, error);
-      return None;
+      None
     }
   }
 }
@@ -150,9 +118,3 @@ async fn parse(feed: Result<Feed, WitherError>) -> Option<Feed> {
     }
   }
 }
-
-// async fn queue_jobs (cursor: Result<Cursor<Subscription>, Error>) -> Result<ObjectId, Error> {
-//   let cursor = match cursor {
-//     Ok(cursor) =>
-//   };
-// }
