fbe3d42ac61628f7f1001899767dacb1c5557939
Nicolas del Valle <nicolas.delvalle@gmail.com>
2022-06-24 00:14:31 -0300

    Update user authentication

--- api/src/authentication/user.rs
+++ api/src/authentication/user.rs
@@ -1,1 +1,41 @@
+// TODO: Move this logic and token logic to a separate authentication file.
+use axum::{
+  async_trait,
+  extract::{FromRequest, RequestParts, TypedHeader},
+  headers::{authorization::Bearer, Authorization},
+};
 
+use crate::errors::AuthenticateError;
+use crate::errors::Error;
+use crate::lib::token;
+use crate::settings::get_settings;
+
+pub struct AuthenticateUser;
+
+#[async_trait]
+impl<B> FromRequest<B> for AuthenticateUser
+where
+  B: Send,
+{
+  type Rejection = Error;
+
+  async fn from_request(req: &mut RequestParts<B>) -> Result<Self, Self::Rejection> {
+    let TypedHeader(Authorization(bearer)) =
+      TypedHeader::<Authorization<Bearer>>::from_request(req)
+        .await
+        .map_err(|_| AuthenticateError::InvalidToken)?;
+
+    let settings = get_settings();
+    let secret = settings.auth.secret.as_str();
+    let token_data =
+      token::decode(bearer.token(), secret).map_err(|_| AuthenticateError::InvalidToken)?;
+
+    let user = token_data.claims.user;
+
+    // Store the user from token in the request extensions to allow accessing it
+    // from other middlewares.
+    req.extensions_mut().insert(user);
+
+    Ok(Self)
+  }
+}
