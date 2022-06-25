18cd962adf903a507fe04d5cc7bc7fd50e4a80a4
Nicolas del Valle <nicolas.delvalle@gmail.com>
2022-06-25 00:23:48 -0300

    Create top cards dashboard stats

--- web/src/pages/app/[app]/dashboard.tsx
+++ web/src/pages/app/[app]/dashboard.tsx
@@ -14,6 +14,7 @@ const AppDashboard: NextPageWithLayout = () => {
       () => null,
       (_app) => (
         <div className="space-y-4">
+          <TopCardsStats />
           <Skeleton className="h-48 w-full rounded-lg" />
           <Skeleton className="h-96 w-full rounded-lg" />
           <Skeleton className="h-24 w-full rounded-lg" />
@@ -36,5 +37,33 @@ AppDashboard.getLayout = (page) => (
     {page}
   </Layout>
 );
+
+const stats = [
+  {name: 'Total subscriptions', stat: '71,897'},
+  {name: 'Total events sent', stat: '341,032'},
+  {name: 'Average sync time', stat: '3 min'},
+];
+
+function TopCardsStats() {
+  return (
+    <div>
+      <dl className="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
+        {stats.map((item) => (
+          <div
+            key={item.name}
+            className="overflow-hidden rounded-lg bg-white px-4 py-5 shadow sm:p-6"
+          >
+            <dt className="truncate text-sm font-medium text-gray-500">
+              {item.name}
+            </dt>
+            <dd className="mt-1 text-3xl font-semibold text-gray-900">
+              {item.stat}
+            </dd>
+          </div>
+        ))}
+      </dl>
+    </div>
+  );
+}
 
 export default AppDashboard;
