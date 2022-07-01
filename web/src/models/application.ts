37b586d13703b17a0bc7cc7a59c759e54e8503c9
Christian Gill <gillchristiang@gmail.com>
2022-07-01 15:54:29 +0200

    Include Members in app patch check

--- web/src/models/application.ts
+++ web/src/models/application.ts
@@ -69,6 +69,7 @@ export const useAppIdFromPath = () =>
       useRouteOfType('AppSubs'),
       useRouteOfType('AppLogs'),
       useRouteOfType('AppSettingsKeys'),
+      useRouteOfType('AppSettingsMembers'),
     ],
     fold(O.getFirstMonoid()),
     O.map(({app}) => app),
