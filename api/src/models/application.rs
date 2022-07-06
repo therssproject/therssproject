04b0e025874dd82cba310f0c5e30280760f85e92
Nicolas del Valle <nicolas.delvalle@gmail.com>
2022-07-05 23:44:01 -0300

    Update application model using owner instead of user

--- api/src/models/application.rs
+++ api/src/models/application.rs
@@ -14,11 +14,11 @@ impl ModelExt for Application {
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize, WitherModel, Validate)]
-#[model(index(keys = r#"doc!{ "user": 1 }"#))]
+#[model(index(keys = r#"doc!{ "owner": 1 }"#))]
 pub struct Application {
   #[serde(rename = "_id", skip_serializing_if = "Option::is_none")]
   pub id: Option<ObjectId>,
-  pub user: ObjectId,
+  pub owner: ObjectId,
   pub name: String,
   pub description: Option<String>,
   pub updated_at: Date,
@@ -26,7 +26,7 @@ pub struct Application {
 }
 
 impl Application {
-  pub fn new<A, B>(user: ObjectId, name: A, description: Option<B>) -> Self
+  pub fn new<A, B>(owner: ObjectId, name: A, description: Option<B>) -> Self
   where
     A: Into<String>,
     B: Into<String>,
@@ -34,7 +34,7 @@ impl Application {
     let now = now();
     Self {
       id: None,
-      user,
+      owner,
       name: name.into(),
       description: description.map(Into::into),
       updated_at: now,
@@ -48,7 +48,7 @@ pub struct PublicApplication {
   #[serde(alias = "_id", serialize_with = "serialize_object_id_as_hex_string")]
   pub id: ObjectId,
   #[serde(serialize_with = "serialize_object_id_as_hex_string")]
-  pub user: ObjectId,
+  pub owner: ObjectId,
   pub name: String,
   pub description: Option<String>,
   #[serde(with = "bson_datetime_as_rfc3339_string")]
@@ -61,7 +61,7 @@ impl From<Application> for PublicApplication {
   fn from(application: Application) -> Self {
     Self {
       id: application.id.unwrap(),
-      user: application.user,
+      owner: application.owner,
       name: application.name,
       description: application.description,
       updated_at: application.updated_at,
