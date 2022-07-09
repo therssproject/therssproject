5921cbacd9cde185fdd00869b60f0116163ada55
Nicolas del Valle <nicolas.delvalle@gmail.com>
2022-07-08 23:36:36 -0300

    Update paginated response implementation

--- api/src/lib/paginated_response.rs
+++ api/src/lib/paginated_response.rs
@@ -3,57 +3,68 @@ use axum::{
   http::StatusCode,
   response::{IntoResponse, Response},
 };
-use serde::{Deserialize, Serialize};
+use serde::Serialize;
+use tracing::error;
 
-#[derive(Serialize, Deserialize)]
-pub struct PaginatedResponse<T: Send> {
+#[derive(Serialize)]
+pub struct PaginatedResponse<T> {
   pub body: T,
   pub count: i64,
   pub offset: i64,
   pub limit: i64,
 }
 
-impl<T: Send> IntoResponse for PaginatedResponse<T>
+impl<T> IntoResponse for PaginatedResponse<T>
 where
   T: Serialize,
 {
   fn into_response(self) -> Response {
-    let count = self.count.to_string();
-    let offset = self.offset.to_string();
-    let limit = self.limit.to_string();
+    let bytes = match serde_json::to_vec(&self.body) {
+      Ok(bytes) => bytes,
+      Err(err) => {
+        error!("failed to serialize paginated response: {}", err);
+        return create_error_response(err);
+      }
+    };
 
-    match serde_json::to_vec(&self.body) {
-      Ok(bytes) => (
-        [
-          (
-            header::CONTENT_TYPE,
-            HeaderValue::from_static(mime::APPLICATION_JSON.as_ref()),
-          ),
-          (
-            HeaderName::from_static("X-Pagination-Count"),
-            HeaderValue::from_str(&count).unwrap(),
-          ),
-          (
-            HeaderName::from_static("X-Pagination-offset"),
-            HeaderValue::from_str(&offset).unwrap(),
-          ),
-          (
-            HeaderName::from_static("X-Pagination-Limit"),
-            HeaderValue::from_str(&limit).unwrap(),
-          ),
-        ],
-        bytes,
-      )
-        .into_response(),
-      Err(err) => (
-        StatusCode::INTERNAL_SERVER_ERROR,
-        [(
+    (
+      [
+        (
           header::CONTENT_TYPE,
-          header::HeaderValue::from_static(mime::TEXT_PLAIN_UTF_8.as_ref()),
-        )],
-        err.to_string(),
-      )
-        .into_response(),
-    }
+          HeaderValue::from_static(mime::APPLICATION_JSON.as_ref()),
+        ),
+        (
+          HeaderName::from_static("X-Pagination-Count"),
+          to_header_value(self.count),
+        ),
+        (
+          HeaderName::from_static("X-Pagination-offset"),
+          to_header_value(self.offset),
+        ),
+        (
+          HeaderName::from_static("X-Pagination-Limit"),
+          to_header_value(self.limit),
+        ),
+      ],
+      bytes,
+    )
+      .into_response()
   }
+}
+
+fn create_error_response(err: impl std::error::Error) -> Response {
+  (
+    StatusCode::INTERNAL_SERVER_ERROR,
+    [(
+      header::CONTENT_TYPE,
+      header::HeaderValue::from_static(mime::TEXT_PLAIN_UTF_8.as_ref()),
+    )],
+    err.to_string(),
+  )
+    .into_response()
+}
+
+fn to_header_value(value: i64) -> HeaderValue {
+  let value = value.to_string();
+  HeaderValue::from_str(&value).unwrap()
 }
