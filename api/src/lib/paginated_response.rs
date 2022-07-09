d38a455c4a47f05ec62fb08befd733bf61f75338
Nicolas del Valle <nicolas.delvalle@gmail.com>
2022-07-08 23:41:45 -0300

    Update paginated response implementation

--- api/src/lib/paginated_response.rs
+++ api/src/lib/paginated_response.rs
@@ -4,7 +4,6 @@ use axum::{
   response::{IntoResponse, Response},
 };
 use serde::Serialize;
-use tracing::error;
 
 #[derive(Serialize)]
 pub struct PaginatedResponse<T> {
@@ -19,49 +18,40 @@ where
   T: Serialize,
 {
   fn into_response(self) -> Response {
-    let bytes = match serde_json::to_vec(&self.body) {
-      Ok(bytes) => bytes,
-      Err(err) => {
-        error!("failed to serialize paginated response: {}", err);
-        return create_error_response(err);
-      }
-    };
-
-    (
-      [
-        (
+    match serde_json::to_vec(&self.body) {
+      Ok(bytes) => (
+        [
+          (
+            header::CONTENT_TYPE,
+            HeaderValue::from_static(mime::APPLICATION_JSON.as_ref()),
+          ),
+          (
+            HeaderName::from_static("X-Pagination-Count"),
+            to_header_value(self.count),
+          ),
+          (
+            HeaderName::from_static("X-Pagination-offset"),
+            to_header_value(self.offset),
+          ),
+          (
+            HeaderName::from_static("X-Pagination-Limit"),
+            to_header_value(self.limit),
+          ),
+        ],
+        bytes,
+      )
+        .into_response(),
+      Err(err) => (
+        StatusCode::INTERNAL_SERVER_ERROR,
+        [(
           header::CONTENT_TYPE,
-          HeaderValue::from_static(mime::APPLICATION_JSON.as_ref()),
-        ),
-        (
-          HeaderName::from_static("X-Pagination-Count"),
-          to_header_value(self.count),
-        ),
-        (
-          HeaderName::from_static("X-Pagination-offset"),
-          to_header_value(self.offset),
-        ),
-        (
-          HeaderName::from_static("X-Pagination-Limit"),
-          to_header_value(self.limit),
-        ),
-      ],
-      bytes,
-    )
-      .into_response()
+          header::HeaderValue::from_static(mime::TEXT_PLAIN_UTF_8.as_ref()),
+        )],
+        err.to_string(),
+      )
+        .into_response(),
+    }
   }
-}
-
-fn create_error_response(err: impl std::error::Error) -> Response {
-  (
-    StatusCode::INTERNAL_SERVER_ERROR,
-    [(
-      header::CONTENT_TYPE,
-      header::HeaderValue::from_static(mime::TEXT_PLAIN_UTF_8.as_ref()),
-    )],
-    err.to_string(),
-  )
-    .into_response()
 }
 
 fn to_header_value(value: i64) -> HeaderValue {
