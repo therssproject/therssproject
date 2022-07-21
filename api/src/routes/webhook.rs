b93315413fa13686a84195cb6b66e7260fad0034
Nicolas del Valle <nicolas.delvalle@gmail.com>
2022-07-20 23:42:17 -0300

    Add 'from' query string support to webhooks route

--- api/src/routes/webhook.rs
+++ api/src/routes/webhook.rs
@@ -1,10 +1,13 @@
-use axum::{extract::Extension, routing::get, Router};
+use axum::{extract::Extension, extract::Query, routing::get, Router};
 use bson::doc;
+use serde::Deserialize;
 use tracing::debug;
 use wither::mongodb::options::FindOptions;
 
+use crate::errors::BadRequest;
 use crate::errors::Error;
 use crate::lib::database_model::ModelExt;
+use crate::lib::date::from_iso;
 use crate::lib::paginated_response::PaginatedJson;
 use crate::models::application::Application;
 use crate::models::webhook::{PublicWebhook, Webhook};
@@ -15,16 +18,22 @@ pub fn create_router() -> Router {
 
 async fn query_webhooks(
   Extension(application): Extension<Application>,
+  Query(query): Query<RequestQuery>,
 ) -> Result<PaginatedJson<Vec<PublicWebhook>>, Error> {
   let application_id = application.id.unwrap();
+  let from = query.from;
 
   let options = FindOptions::builder()
-    .sort(doc! { "created_at": -1 })
+    .sort(doc! { "created_at": -1_i32 })
     .limit(50)
     .build();
 
-  let (webhooks, count) =
-    Webhook::find_and_count(doc! { "application": &application_id}, Some(options)).await?;
+  let mut query = doc! { "application_id": application_id };
+  if let Some(from) = from {
+    query.insert("created_at", doc! { "$gte": to_date(from)? });
+  }
+
+  let (webhooks, count) = Webhook::find_and_count(query, Some(options)).await?;
 
   let webhooks = webhooks
     .into_iter()
@@ -40,4 +49,16 @@ async fn query_webhooks(
   };
 
   Ok(res)
+}
+
+#[derive(Deserialize)]
+struct RequestQuery {
+  from: Option<String>,
+}
+
+fn to_date<A>(iso: A) -> Result<chrono::DateTime<chrono::Utc>, BadRequest>
+where
+  A: AsRef<str>,
+{
+  from_iso(iso.as_ref()).map_err(|_e| BadRequest::new("from", "Invalid ISO string date"))
 }
