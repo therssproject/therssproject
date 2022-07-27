72a882399400ab5aa0912ccf89bca7d88244766e
gillchristian <gillchristiang@gmail.com>
2022-07-27 15:28:49 +0200

    web: button styles

--- web/src/components/buttons/Button.tsx
+++ web/src/components/buttons/Button.tsx
@@ -3,7 +3,7 @@ import {ImSpinner2} from 'react-icons/im';
 
 import {clsxm} from '@/lib/clsxm';
 
-type ButtonVariant = 'primary' | 'outline' | 'ghost' | 'light' | 'dark';
+export type ButtonVariant = 'primary' | 'outline' | 'ghost' | 'light' | 'dark';
 
 type Props = {
   isLoading?: boolean;
@@ -12,13 +12,73 @@ type Props = {
   variant?: ButtonVariant;
 } & React.ComponentPropsWithRef<'button'>;
 
+type ButtonClsxm = {
+  variant: ButtonVariant;
+  isDarkBg: boolean;
+  isLoading: boolean;
+  isFullWidth: boolean;
+  className: string;
+};
+
+export const buttonClsxm = ({
+  variant,
+  isDarkBg,
+  isLoading,
+  isFullWidth,
+  className,
+}: ButtonClsxm) =>
+  clsxm(
+    'inline-flex items-center rounded-md px-3 py-2 shadow-sm',
+    'text-sm font-medium leading-4',
+    'focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2',
+    'border border-transparent',
+    'transition-colors duration-75',
+    [
+      variant === 'primary' && [
+        'bg-cyan-500 text-white',
+        'focus-visible:ring-cyan-500',
+        'hover:bg-cyan-600 hover:text-white',
+        'active:bg-cyan-500',
+        'disabled:bg-cyan-400 disabled:hover:bg-cyan-400',
+      ],
+      variant === 'outline' && [
+        'text-cyan-500',
+        'focus-visible:ring-cyan-500',
+        'hover:bg-cyan-50 active:bg-cyan-100 disabled:bg-cyan-100',
+        isDarkBg && 'hover:bg-gray-900 active:bg-gray-800 disabled:bg-gray-800',
+      ],
+      variant === 'ghost' && [
+        'text-cyan-500',
+        'shadow-none',
+        'hover:bg-cyan-50 active:bg-cyan-100 disabled:bg-cyan-100',
+        isDarkBg && 'hover:bg-gray-900 active:bg-gray-800 disabled:bg-gray-800',
+      ],
+      variant === 'light' && [
+        'bg-white text-dark ',
+        'focus-visible:ring-gray-200',
+        'hover:bg-gray-100 hover:text-dark',
+        'active:bg-white/80 disabled:bg-gray-200',
+      ],
+      variant === 'dark' && [
+        'bg-gray-900 text-white',
+        'focus-visible:ring-cyan-900',
+        'hover:bg-gray-800 active:bg-gray-700 disabled:bg-gray-700',
+      ],
+    ],
+    'disabled:cursor-not-allowed',
+    isLoading &&
+      'relative text-transparent transition-none hover:text-transparent disabled:cursor-wait',
+    isFullWidth && 'flex w-full justify-center',
+    className,
+  );
+
 export const Button = React.forwardRef<HTMLButtonElement, Props>(
   (
     {
       children,
-      className,
+      className = '',
       disabled: buttonDisabled,
-      isLoading,
+      isLoading = false,
       variant = 'primary',
       isDarkBg = false,
       isFullWidth = false,
@@ -33,51 +93,13 @@ export const Button = React.forwardRef<HTMLButtonElement, Props>(
         ref={ref}
         type="button"
         disabled={disabled}
-        className={clsxm(
-          'inline-flex items-center rounded px-4 py-2 font-semibold',
-          'focus:outline-none focus-visible:ring focus-visible:ring-primary-500',
-          'shadow-sm',
-          'transition-colors duration-75',
-          [
-            variant === 'primary' && [
-              'bg-primary-500 text-white',
-              'border border-primary-600',
-              'hover:bg-primary-600 hover:text-white',
-              'active:bg-primary-500',
-              'disabled:bg-primary-400 disabled:hover:bg-primary-400',
-            ],
-            variant === 'outline' && [
-              'text-primary-500',
-              'border border-primary-500',
-              'hover:bg-primary-50 active:bg-primary-100 disabled:bg-primary-100',
-              isDarkBg &&
-                'hover:bg-gray-900 active:bg-gray-800 disabled:bg-gray-800',
-            ],
-            variant === 'ghost' && [
-              'text-primary-500',
-              'shadow-none',
-              'hover:bg-primary-50 active:bg-primary-100 disabled:bg-primary-100',
-              isDarkBg &&
-                'hover:bg-gray-900 active:bg-gray-800 disabled:bg-gray-800',
-            ],
-            variant === 'light' && [
-              'bg-white text-dark ',
-              'border border-gray-300',
-              'hover:bg-gray-100 hover:text-dark',
-              'active:bg-white/80 disabled:bg-gray-200',
-            ],
-            variant === 'dark' && [
-              'bg-gray-900 text-white',
-              'border border-gray-600',
-              'hover:bg-gray-800 active:bg-gray-700 disabled:bg-gray-700',
-            ],
-          ],
-          'disabled:cursor-not-allowed',
-          isLoading &&
-            'relative text-transparent transition-none hover:text-transparent disabled:cursor-wait',
-          isFullWidth && 'flex w-full justify-center',
+        className={buttonClsxm({
+          variant,
+          isDarkBg,
+          isFullWidth,
+          isLoading,
           className,
-        )}
+        })}
         {...rest}
       >
         {isLoading && (
@@ -87,7 +109,7 @@ export const Button = React.forwardRef<HTMLButtonElement, Props>(
               {
                 'text-white': ['primary', 'dark'].includes(variant),
                 'text-black': ['light'].includes(variant),
-                'text-primary-500': ['outline', 'ghost'].includes(variant),
+                'text-cyan-500': ['outline', 'ghost'].includes(variant),
               },
             )}
           >
