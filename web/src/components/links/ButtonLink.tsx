c9e9971239af179c5d2fd5d0cb94033e40329015
gillchristian <gillchristiang@gmail.com>
2022-07-27 15:28:49 +0200

    web: button styles

--- web/src/components/links/ButtonLink.tsx
+++ web/src/components/links/ButtonLink.tsx
@@ -1,13 +1,11 @@
 import * as React from 'react';
 
-import {clsxm} from '@/lib/clsxm';
-
 import {
   Props as UnstyledLinkProps,
   UnstyledLink,
 } from '@/components/links/UnstyledLink';
 
-type ButtonVariant = 'primary' | 'outline' | 'ghost' | 'light' | 'dark';
+import {buttonClsxm, ButtonVariant} from '../buttons/Button';
 
 type Props = {
   isDarkBg?: boolean;
@@ -16,55 +14,20 @@ type Props = {
 
 export const ButtonLink = React.forwardRef<HTMLAnchorElement, Props>(
   (
-    {children, className, variant = 'primary', isDarkBg = false, ...rest},
+    {children, className = '', variant = 'primary', isDarkBg = false, ...rest},
     ref,
   ) => {
     return (
       <UnstyledLink
         ref={ref}
         {...rest}
-        className={clsxm(
-          'inline-flex items-center rounded px-4 py-2 font-semibold',
-          'focus:outline-none focus-visible:ring focus-visible:ring-primary-500',
-          'shadow-sm',
-          'transition-colors duration-75',
-          [
-            variant === 'primary' && [
-              'bg-primary-500 text-white',
-              'border border-primary-600',
-              'hover:bg-primary-600 hover:text-white',
-              'active:bg-primary-500',
-              'disabled:bg-primary-400 disabled:hover:bg-primary-400',
-            ],
-            variant === 'outline' && [
-              'text-primary-500',
-              'border border-primary-500',
-              'hover:bg-primary-50 active:bg-primary-100 disabled:bg-primary-100',
-              isDarkBg &&
-                'hover:bg-gray-900 active:bg-gray-800 disabled:bg-gray-800',
-            ],
-            variant === 'ghost' && [
-              'text-primary-500',
-              'shadow-none',
-              'hover:bg-primary-50 active:bg-primary-100 disabled:bg-primary-100',
-              isDarkBg &&
-                'hover:bg-gray-900 active:bg-gray-800 disabled:bg-gray-800',
-            ],
-            variant === 'light' && [
-              'bg-white text-dark ',
-              'border border-gray-300',
-              'hover:bg-gray-100 hover:text-dark',
-              'active:bg-white/80 disabled:bg-gray-200',
-            ],
-            variant === 'dark' && [
-              'bg-gray-900 text-white',
-              'border border-gray-600',
-              'hover:bg-gray-800 active:bg-gray-700 disabled:bg-gray-700',
-            ],
-          ],
-          'disabled:cursor-not-allowed',
+        className={buttonClsxm({
+          variant,
+          isDarkBg,
+          isFullWidth: false,
+          isLoading: false,
           className,
-        )}
+        })}
       >
         {children}
       </UnstyledLink>
