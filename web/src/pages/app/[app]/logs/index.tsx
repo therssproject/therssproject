3f718da749ff7fc54c39675bfe0e6e597b4bf036
gillchristian <gillchristiang@gmail.com>
2022-07-27 10:54:24 +0200

    web: logs empty state

--- web/src/pages/app/[app]/logs/index.tsx
+++ web/src/pages/app/[app]/logs/index.tsx
@@ -1,3 +1,4 @@
+import {ViewListIcon} from '@heroicons/react/outline';
 import {sequenceT} from 'fp-ts/Apply';
 import * as A from 'fp-ts/Array';
 import {pipe} from 'fp-ts/function';
@@ -6,9 +7,10 @@ import React from 'react';
 import * as RD from 'remote-data-ts';
 
 import {useAtom} from '@/lib/jotai';
+import {Route} from '@/lib/routes';
 
-import {Alert} from '@/components/Alert';
 import {Layout} from '@/components/layout/Layout';
+import {PrimaryLink} from '@/components/links/PrimaryLink';
 import {Skeleton} from '@/components/Skeleton';
 
 import {LogItem} from '@/features/LogItem';
@@ -30,11 +32,11 @@ const AppLogs: NextPageWithLayout = () => {
     RD.match({
       notAsked: () => <Skeleton className="h-48 w-full rounded-lg" />,
       loading: () => <Skeleton className="h-48 w-full rounded-lg" />,
-      success: ([_app, logs]) =>
+      success: ([app, logs]) =>
         pipe(
           logs,
           A.match(
-            () => <Alert>No logs received yet ...</Alert>,
+            () => <EmptyState app={app.id} />,
             (logs) => (
               <div className="overflow-hidden bg-white shadow sm:rounded-md">
                 <ul role="list" className="divide-y divide-gray-200">
@@ -50,6 +52,23 @@ const AppLogs: NextPageWithLayout = () => {
     }),
   );
 };
+
+const EmptyState = ({app}: {app: string}) => (
+  <div className="mt-16">
+    <div className="text-center">
+      <ViewListIcon className="mx-auto h-12 w-12 text-gray-400" />
+      <h3 className="mt-2 text-sm font-medium text-gray-900">
+        No webhook events sent yet
+      </h3>
+      <p className="mt-1 text-sm text-gray-500">
+        Webhook events will show here once new entries are added
+        <br />
+        to your feed{' '}
+        <PrimaryLink href={Route.appSubs(app)}>Subscriptions</PrimaryLink>.
+      </p>
+    </div>
+  </div>
+);
 
 AppLogs.getLayout = (page) => (
   <Layout variant="applications" title="Logs" seo={{}}>
