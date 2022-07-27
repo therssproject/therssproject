28fdc94615aa7ff7a5b297b0ddc46d8e82aec599
gillchristian <gillchristiang@gmail.com>
2022-07-27 11:02:14 +0200

    web: developers image

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -9,7 +9,7 @@ import {
   ShieldCheckIcon,
   XIcon,
 } from '@heroicons/react/outline';
-import {ChevronRightIcon, ExternalLinkIcon} from '@heroicons/react/solid';
+import {ChevronRightIcon} from '@heroicons/react/solid';
 import {pipe} from 'fp-ts/function';
 import * as O from 'fp-ts/Option';
 import {useAtom} from 'jotai';
@@ -701,7 +701,7 @@ const HomePage: NextPageWithLayout = () => {
             <div className="relative h-56 bg-indigo-600 sm:h-72 md:absolute md:left-0 md:h-full md:w-1/2">
               <img
                 className="h-full w-full object-cover"
-                src="https://images.unsplash.com/photo-1525130413817-d45c1d127c42?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1920&q=60&sat=-100"
+                src="/images/developers.jpg"
                 alt=""
               />
               <div
@@ -712,10 +712,10 @@ const HomePage: NextPageWithLayout = () => {
             <div className="relative mx-auto max-w-md px-4 py-12 sm:max-w-7xl sm:px-6 sm:py-20 md:py-28 lg:px-8 lg:py-32">
               <div className="md:ml-auto md:w-1/2 md:pl-10">
                 <h2 className="text-base font-semibold uppercase tracking-wider text-gray-300">
-                  Award winning support
+                  Made by indie hackers
                 </h2>
                 <p className="mt-2 text-3xl font-extrabold tracking-tight text-white sm:text-4xl">
-                  We’re here to help
+                  For developers, by developers
                 </p>
                 <p className="mt-3 text-lg text-gray-300">
                   Lorem ipsum dolor sit amet, consectetur adipiscing elit. Et,
@@ -725,16 +725,12 @@ const HomePage: NextPageWithLayout = () => {
                 </p>
                 <div className="mt-8">
                   <div className="inline-flex rounded-md shadow">
-                    <a
-                      href="#"
+                    <UnstyledLink
+                      href={external('https://github.com/therssproject/documentation')}
                       className="inline-flex items-center justify-center rounded-md border border-transparent bg-white px-5 py-3 text-base font-medium text-gray-900 hover:bg-gray-50"
                     >
-                      Visit the help center
-                      <ExternalLinkIcon
-                        className="-mr-1 ml-3 h-5 w-5 text-gray-400"
-                        aria-hidden="true"
-                      />
-                    </a>
+                      Visit the documentation repo
+                    </UnstyledLink>
                   </div>
                 </div>
               </div>
