c0ed718393ce77005e7df4ae39d3dc79d8468164
Christian Gill <gillchristiang@gmail.com>
2022-09-12 23:03:37 +0200

    Remove comment

--- web/src/config.ts
+++ web/src/config.ts
@@ -1,6 +1,5 @@
 export const API_URL =
   process.env.NEXT_PUBLIC_BASE_URL ?? 'http://localhost:8080';
-// process.env.NEXT_PUBLIC_BASE_URL ?? 'https://api.therssproject.com';
 
 export const MixPanelToken =
   process.env.NEXT_PUBLIC_MIXPANEL_TOKEN ?? 'de87044fcb833916e18863bda66fa711';
